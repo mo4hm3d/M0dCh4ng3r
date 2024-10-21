dbfed59fe7284f94d85e68f6ab44b4a677492553
mohammadbinibrahim <109195335+mohammadbinibrahim@users.noreply.github.com>
2024-10-21 14:24:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ print("*************************I AM NOT RESPONSIBLE FOR ANY ILLEGAL USE********
 
   
 print("Setting Up.......")
-os.system("sudo chown root:root m0dch4ng3r.py")
+os.system("sudo chown root:root m4cch4ng3r.py")
 os.system("sudo apt install python3-pip")
 os.system("pip3 install getmac")
 time.sleep(2)
@@ -17,6 +17,6 @@ time.sleep(2)
 os.system("pip3 install os")
 time.sleep(2)
 print("Finishing Up.......")
-os.system("sudo chmod 700 m0dch4ng3r.py")
+os.system("sudo chmod 700 m4cch4ng3r.py")
 time.sleep(2)
 print("Finished Setting Up!")
