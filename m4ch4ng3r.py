fd966cc191c3011eaee9889aae2af1a9b181bd55
mohammadbinibrahim <109195335+mohammadbinibrahim@users.noreply.github.com>
2024-11-29 15:39:44 +0200

    Update m4ch4ng3r.py

--- m4ch4ng3r.py
+++ m4ch4ng3r.py
@@ -16,7 +16,7 @@ if os.geteuid() != 0:
 
 try:
  def Macchanger():
-   print(f"[Your MAC Address Is]: {gmc()}")
+   print(f"[Your MAC address is]: {gmc()}")
    os.system(f"ifconfig {args.interface} down")
    os.system(f"ifconfig {args.interface} hw ether {args.mac}")
    os.system(f"ifconfig {args.interface} up")
@@ -24,20 +24,20 @@ try:
  Macchanger()
  option = ""
  while option != "yes" or option != "no" or option != "Yes" or option != "No":
-   option = input("[Do You Want To Change Your MAC Again]: ")
+   option = input("[Do you want to spoof your MAC address again]: ")
    if option == "Yes" or option == "yes" or option == "YES":
-    desired_mac = str(input("[Enter MAC Address]: "))
+    desired_mac = str(input("[Enter MAC address]: "))
     os.system(f"ifconfig {args.interface} down")
     os.system(f"ifconfig {args.interface} hw ether {desired_mac}")
     os.system(f"ifconfig {args.interface} up")
-    print(f"[Your MAC Address Was Sucessfully Changed To]: {desired_mac}")
+    print(f"[Your MAC address was sucessfully changed to]: {desired_mac}")
    elif option == "No" or option == "no" or option == "NO":
-    exit("\nThanks For Using M0dCh4ng3r")
+    exit("\nThanks For using M4Ch4ng3r")
    elif option == "exit" or option == "quit":
-       exit("\nThanks For Using M0dCh4ng3r")
+       exit("\nThanks for using M4Ch4ng3r")
    elif option == "ifconfig" or option == "ipconfig" or option == "ip":
        os.system("ifconfig")
    else:
-    print("Invalid Command!")
+    print("Invalid command!")
 except KeyboardInterrupt:
-   exit("\nThanks For Using M4Ch4ng3r")
+   exit("\nThanks for using M4Ch4ng3r")
